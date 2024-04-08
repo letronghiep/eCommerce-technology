66c5af49a25c94dc3fa003bcda4560c2444ab6e9
letronghiep <letronghiep213@gmail.com>
2024-04-08 22:34:11 +0700

    Update controller product

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -2,13 +2,11 @@
 const express = require("express");
 const {
   createCategory,
-  getCategoryChild,
-  getParentCategory,
+  getAllCategories
 } = require("../controllers/category.controller");
 
 const router = express.Router();
 
 router.post("/", createCategory);
-router.get("/parent", getParentCategory);
-router.get("/child", getCategoryChild);
+router.get("/", getAllCategories);
 module.exports = router;
